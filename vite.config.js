7448a40e3ba82fd2b99bdd7836509598bd598569
imzyb <vanness.x@gmail.com>
2025-06-13 13:25:23 +0800

    fix: Define all required path aliases in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,14 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    // 在这里定义所有路径别名
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '$api': path.resolve(__dirname, './src/lib/api.js'),
+      '$stores': path.resolve(__dirname, './src/lib/stores.js'),
+      '$utils': path.resolve(__dirname, './src/lib/utils.js'),
+      '$components': path.resolve(__dirname, './src/lib/components'),
+      '$icons': path.resolve(__dirname, './src/lib/icons'),
     }
   },
   server: {
@@ -18,7 +24,13 @@ export default defineConfig({
       '/sub': {
         target: 'http://127.0.0.1:8787',
         changeOrigin: true,
-      }
+      },
+      // 如果你的 mytoken 不是 'auto'，也需要为它添加代理
+      // 例如，如果 mytoken 是 'mysecrettoken'
+      // '/mysecrettoken': {
+      //   target: 'http://127.0.0.1:8787',
+      //   changeOrigin: true,
+      // }
     }
   }
 })
